44d916991f5b603d968abc84064e68183cff96e4
DanyalNazir10 <danyal.nazir@mergestack.com>
2023-11-14 03:54:47 -0800

    Added age verification logic

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -3,11 +3,31 @@ import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
 const Form = () => {
-  const { register, control, handleSubmit, formState: {errors} } = useForm();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const calculateAge = (dob) => {
+    const today = new Date();
+    let age = today.getFullYear() - dob.getFullYear();
+    return age;
+  };
 
   const validateAge = (data) => {
-    console.log("Form Submitted with data: ", data);
+    const { birthDay, birthMonth, birthYear } = data;
+    const dob = new Date(`${birthYear}-${birthMonth}-${birthDay}`);
+    const age = calculateAge(dob);
+
+    if (age < 15) {
+      alert(`People of your age ${age} is restricted to accees GOOGLE.`);
+    } else {
+      window.location.href = "https://www.google.com";
+    }
   };
+
   return (
     <>
       <form onSubmit={handleSubmit(validateAge)}>
@@ -15,64 +35,64 @@ const Form = () => {
           <legend>Enter Your Date of Birth</legend>
 
           <div className="form-items">
-          <label htmlFor="birthDay">Birth Day</label>
-          <input
-            type="number"
-            id="birthDay"
-            {...register("birthDay", {
+            <label htmlFor="birthDay">Birth Day</label>
+            <input
+              type="number"
+              id="birthDay"
+              {...register("birthDay", {
                 required: "Birth day is required",
-                min:{
-                    value: 1,
-                    message: "Birth day should be between 1 and 31"
-                }, 
-                max:{
-                    value: 31,
-                    message: "Birth day should be between 1 and 31"
-                }
-            })}
-            placeholder="Enter your birth day"
-          />
-          <span className="error-message">{errors.birthDay?.message}</span>
+                min: {
+                  value: 1,
+                  message: "Birth day should be between 1 and 31",
+                },
+                max: {
+                  value: 31,
+                  message: "Birth day should be between 1 and 31",
+                },
+              })}
+              placeholder="Enter your birth day"
+            />
+            <span className="error-message">{errors.birthDay?.message}</span>
           </div>
           <div className="form-items">
-          <label htmlFor="birthMonth">Birth Month</label>
-          <input
-            type="number"
-            id="birthMonth"
-            {...register("birthMonth", {
+            <label htmlFor="birthMonth">Birth Month</label>
+            <input
+              type="number"
+              id="birthMonth"
+              {...register("birthMonth", {
                 required: "Birth month is required",
-                min:{
-                    value: 1,
-                    message: "Birth month should be between 1 and 12"
-                }, 
-                max:{
-                    value: 12,
-                    message: "Birth month should be between 1 and 12"
-                }
-            })}
-            placeholder="Enter your birth month"
-          />
-          <span className="error-message">{errors.birthMonth?.message}</span>
+                min: {
+                  value: 1,
+                  message: "Birth month should be between 1 and 12",
+                },
+                max: {
+                  value: 12,
+                  message: "Birth month should be between 1 and 12",
+                },
+              })}
+              placeholder="Enter your birth month"
+            />
+            <span className="error-message">{errors.birthMonth?.message}</span>
           </div>
           <div className="form-items">
-          <label htmlFor="birthYear">Birth Year</label>
-          <input
-            type="number"
-            id="birthYear"
-            {...register("birthYear", {
+            <label htmlFor="birthYear">Birth Year</label>
+            <input
+              type="number"
+              id="birthYear"
+              {...register("birthYear", {
                 required: "Birth year is required",
-                min:{
-                    value: 2000,
-                    message: "Birth month should be between 2000 and 2023"
-                }, 
-                max:{
-                    value: 2023,
-                    message: "Birth month should be between 2000 and 2023"
-                }
-            })}
-            placeholder="Enter your birth year"
-          />
-          <span className="error-message">{errors.birthYear?.message}</span>
+                min: {
+                  value: 2000,
+                  message: "Birth month should be between 2000 and 2023",
+                },
+                max: {
+                  value: 2023,
+                  message: "Birth month should be between 2000 and 2023",
+                },
+              })}
+              placeholder="Enter your birth year"
+            />
+            <span className="error-message">{errors.birthYear?.message}</span>
           </div>
           <input type="submit" value={"Submit"} />
         </fieldset>
